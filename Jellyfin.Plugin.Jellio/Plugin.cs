b677a5bd5afd562b330e3f5f41b3c766143c700f
InfiniteAvenger <calebewest02@gmail.com>
2025-11-05 20:29:24 -0600

    fix: remove embedded config page, users should access /jellio directly

--- Jellyfin.Plugin.Jellio/Plugin.cs
+++ Jellyfin.Plugin.Jellio/Plugin.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
-using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.Jellio;
 
-public class Plugin : BasePlugin<BasePluginConfiguration>, IHasWebPages
+public class Plugin : BasePlugin<BasePluginConfiguration>
 {
     public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
         : base(applicationPaths, xmlSerializer)
@@ -20,16 +18,4 @@ public class Plugin : BasePlugin<BasePluginConfiguration>, IHasWebPages
     public override Guid Id => Guid.Parse("e874be83-fe36-4568-abac-f5ce0574b409");
 
     public static Plugin? Instance { get; private set; }
-
-    public IEnumerable<PluginPageInfo> GetPages()
-    {
-        return
-        [
-            new PluginPageInfo
-            {
-                Name = Name,
-                EmbeddedResourcePath = "Jellyfin.Plugin.Jellio.Web.config.html",
-            },
-        ];
-    }
 }
